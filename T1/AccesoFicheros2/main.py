cf8e121343a4dd382452a34fc46b63e7e60b1fa3
AlexAguado24 <aaguado131@gmail.com>
2022-11-04 14:20:44 +0100

    .

--- T1/AccesoFicheros2/main.py
+++ T1/AccesoFicheros2/main.py
@@ -55,7 +55,7 @@ f.write("Este es mi primer progrma de fichero y lo he escrito en python usando l
 f.close()
 '''
 
-lista = ["\nEscribiendo \n", "listas \n", "en ficheros"]
+#lista = ["\nEscribiendo \n", "listas \n", "en ficheros"]
 
 #lo abrimos como Reescribir (r+)
 '''
@@ -66,11 +66,125 @@ with open ("ficheroenpython.txt", "r+") as f:
     f.writelines(lista)
 f.close()
 '''
+'''
 with open ("ficheroenpython.txt", "a") as f:
     #lo abrimos utilizando read y lo almacenamos en la variable
     #contenido = f.read()
     #escribimos la lista en el archivo linea a linea
     f.writelines(lista)
 f.close()
+'''
+'''
+#Forma clasica de crear un archivo: #Creamos el archivo
+f = open("archivotext-excepciones.txt", "w")
+#Escribimos en el
+f.write("Probando excepciones")
+#Cerramos el archivo
+f.close()
+#Ahora intentamos abrirlo e imprimir su contenido:
+nombre_fichero = "archivotext-excepciones.txt"
+try:
+    with open(nombre_fichero, "r") as f:
+        print(f.read())
+except FileExistsError:
+    print('No existe el fichero con la tabla del')
+'''
+#Ejercicio 1
+#numero = int(input("Escribe un numero positivo del 1 al 100\n"))
+
+'''
+f = open("ejercicio1-100.txt", "w")
+f.write(numero)
+f.close()
+'''
+'''
+nombre_fichero = "ejercicio1-100.txt"
+with open(nombre_fichero, "w") as f:
+    f.write(str(numero))
+f.close()
+'''
+'''
+¿Por que for y no while?
+Por que sabemos el numero de veces que queremos que se ejecute la accion,
+si no lo supieramos usariamos while
+'''
+#Ejercicio 2
+'''
+numero = int(input("Escribe un numero positivo del 1 al 10\n"))
+fichero2 = "ejercicio2-tabla.txt"
+
+with open(fichero2, "w") as f:
+    for i in range(1,11):
+        f.write(str(i)+"x"+str(numero)+"="+str(i*numero)+"\n")
+f.close()
+'''
+
+#Ejercicio 3
+'''
+numero = int(input("Escribe un numero positivo del 1 al 10\n"))
+fichero3 = "ejercicio3-tabla.txt"
+
+with open(fichero3, "w") as f:
+    for i in range(1,numero+1):
+        for n in range(0,11):
+            f.write(str(n)+"x"+str(i)+"="+str(n*i)+"\n")
+        f.write("\n")
+f.close()
+'''
+
+#Ejercicio4
+'''
+numero = int(input("Escribe un numero positivo del 1 al 10\n"))
+fichero3 = "ejercicio3-tabla del "+str(numero)+".txt "
+
+with open(fichero3, "w") as f:
+    for i in range(1,numero+1):
+        for n in range(0,11):
+            f.write(str(n)+"x"+str(i)+"="+str(n*i)+"\n")
+        f.write("\n")
+f.close()
+'''
+
+#Ejercicio5
+'''
+numero = int(input("Escribe un numero positivo del 1 al 10\n"))
+ficheroabrir = "fichero-tabla-del-"+str(numero)+".txt"
+try:
+    with open(ficheroabrir, "r") as f:
+        print(f.read())
+except FileNotFoundError:
+    print("fichero no encontrado")
+'''
+
+#Ejercicio6
+'''
+#opcion2
+numero = int(input("Escribe un numero\n"))
+with open("ejercicio2-tabla.txt", "r") as f:
+    texto=(linea for i,linea in enumerate(f) if i>=0 and i<=numero-1)
+    for linea in texto:
+        print(linea)
+'''
+#opcion1
+n=int(input("dame un n de lineas:"))
+with open("ejercicio2-tabla.txt","r") as f:
+    for i in range(n):
+        print(f.readline())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
