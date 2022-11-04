e1fc9e0c17fbeb28ee1220be25385c93d3376636
AlexAguado24 <aaguado131@gmail.com>
2022-11-04 09:30:47 +0100

    acceso a ficheros inicio

---
+++ T1/AccesoFicheros/.idea/clase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#-*- codigo: utf-8 -*-
+#Forma clásica de crear un archivo:
+#creamos el archivo
+f = open("archivotext.txt", "w")
+#Ecribimos en el
+f.write("Creando archivo de texto en python de forma clásica")
+#Cerramos el archivo
+f.close()
