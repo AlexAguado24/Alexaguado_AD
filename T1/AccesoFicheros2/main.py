18a0e595c29033206a19ca6aa4eb1e54804f5bcc
AlexAguado24 <aaguado131@gmail.com>
2022-11-04 11:32:27 +0100

    .

--- T1/AccesoFicheros2/main.py
+++ T1/AccesoFicheros2/main.py
@@ -44,41 +44,33 @@ with open ("ficheroenpython.txt", "r") as f:
     for linea in texto:
         print (linea)
 '''
-#abrimos usando Read (r)
 '''
-with open ("ficheroenpython.txt", "r") as f:
+f = open("ficheroenpython.txt", "w")
 
-    #Lo abrimos utilizando el metodo readlines y lo almacenamos an la variable
-    contenido = f.readlines()#lee todas las lineas y las separa con un \n
-
-    #imprimimos la variable que tiene el contenido
-    print (contenido)
-'''
-'''
-#escribimos despues de leer
-with open("archivotext.txt","r+") as f:
-    #contenido = f.read()
-    f.write("Escribiendo una nueva linea al final del texto")
-f.close()
-'''
-'''
-#abrimos el fichero usando el modo (a)
-with open("archivotext.txt","a") as f:
-    f.write("\notra opcion")
-'''
-'''
-with open("archivotext.txt","a") as f:
-    for iterador in range(8):
-        f.write("\notra opcion")
+f.write("Este es mi primer progrma de fichero y lo he escrito en python usando la \n"
+        "forma clasica, que es la siguiente \n"
+        "f = open() \n"
+        "f.write \n"
+        "f.close")
 f.close()
 '''
 
 lista = ["\nEscribiendo \n", "listas \n", "en ficheros"]
 
 #lo abrimos como Reescribir (r+)
+'''
 with open ("ficheroenpython.txt", "r+") as f:
     #lo abrimos utilizando read y lo almacenamos en la variable
     contenido = f.read()
     #escribimos la lista en el archivo linea a linea
     f.writelines(lista)
 f.close()
+'''
+with open ("ficheroenpython.txt", "a") as f:
+    #lo abrimos utilizando read y lo almacenamos en la variable
+    #contenido = f.read()
+    #escribimos la lista en el archivo linea a linea
+    f.writelines(lista)
+f.close()
+
+
